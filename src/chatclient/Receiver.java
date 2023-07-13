bbc2e99917d91331040a6419268b7794e3e233a4
zhekai-zhao <zhekai1@ualberta.ca>
2023-07-13 13:21:50 -0600

    sender and receiver FT

--- src/chatclient/Receiver.java
+++ src/chatclient/Receiver.java
@@ -1,76 +1,60 @@
 package chatclient;
-
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Base64;
 
 public class Receiver {
+    private ServerSocket serverSocket;
     private Socket socket;
-    private BufferedReader bufferedReader;
+    private DataInputStream inputStream;
 
-    public Receiver(Socket socket) throws IOException {
-        this.socket = socket;
-        this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    public Receiver(int port) throws IOException {
+        this.serverSocket = new ServerSocket(port);
+        this.socket = serverSocket.accept();
+        this.inputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
     }
 
     public void listenForMessages() {
-        String message;
         try {
-            while ((message = bufferedReader.readLine()) != null) {
-                if (message.startsWith("FILE_TRANSFER_REQUEST")) {
-                    String[] parts = message.split(" ");
-                    String fileName = parts[1];
-                    long fileSize = Long.parseLong(parts[2]);
-                    receiveFile(fileName, fileSize);
-                    System.out.println("File received.");
-                    String confirmationMessage = bufferedReader.readLine();
-                    if ("FILE_TRANSFER_COMPLETE".equals(confirmationMessage)) {
-                        System.out.println("File transfer complete.");
-                    }
-                } else {
+            while (true) {
+                int command = inputStream.readInt();
+
+                if (command == 1) {  // This is a message
+                    String message = inputStream.readUTF();
                     System.out.println("Received message: " + message);
+                } else if (command == 2) {  // This is a file
+                    receiveFile();
+                    System.out.println("File received.");
                 }
             }
         } catch (IOException e) {
             System.err.println("Error occurred while reading from the socket: " + e.getMessage());
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                System.err.println("Error occurred while closing the socket: " + e.getMessage());
-            }
         }
     }
 
-    public void receiveFile(String fileName, long fileSize) throws IOException {
+    public void receiveFile() throws IOException {
+        String fileName = inputStream.readUTF();
+        long fileSize = inputStream.readLong();
+        byte[] fileData = new byte[(int) fileSize];
+        inputStream.readFully(fileData);
+
         File file = new File(fileName);
         FileOutputStream fos = new FileOutputStream(file);
-
-        long totalRead = 0;
-        long remaining = fileSize;
-
-        String line;
-        while(remaining > 0 && (line = bufferedReader.readLine()) != null) {
-            if ("FILE_TRANSFER_COMPLETE".equals(line)) {
-                System.out.println("File transfer complete.");
-                break;
-            }
-            byte[] decodedBytes = Base64.getDecoder().decode(line);
-            int decodedLength = decodedBytes.length;
-            totalRead += decodedLength;
-            remaining -= decodedLength;
-            System.out.println("read " + totalRead + " bytes.");
-            fos.write(decodedBytes);
-        }
-
+        fos.write(fileData);
         fos.close();
+    }
+
+    public void close() throws IOException {
+        inputStream.close();
+        socket.close();
+        serverSocket.close();
     }
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 6667);
-            Receiver receiver = new Receiver(socket);
+            Receiver receiver = new Receiver(1234);
             receiver.listenForMessages();
+            receiver.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/chatclient/Sender.java
+++ src/chatclient/Sender.java
@@ -2,69 +2,53 @@ package chatclient;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Arrays;
-import java.util.Base64;
 
 public class Sender {
-    private static final int PACKET_SIZE = 1024;
     private Socket socket;
-    private OutputStream outputStream;
+    private DataOutputStream outputStream;
 
-    public Sender(Socket socket) throws IOException {
-        this.socket = socket;
-        this.outputStream = socket.getOutputStream();
+    public Sender(String address, int port) throws IOException {
+        this.socket = new Socket(address, port);
+        this.outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
     }
 
     public void sendFile(String filePath) throws IOException {
         File file = new File(filePath);
-        if (!file.exists() || !file.isFile()) {
-            System.out.println("File does not exist or is not a regular file.");
-            return;
-        }
-
-        // Send file transfer request
-        String fileInfo = "FILE_TRANSFER_REQUEST " + file.getName() + " " + file.length() + "\n"; 
-        outputStream.write(fileInfo.getBytes());
-        // Send file
         FileInputStream fis = new FileInputStream(file);
-        byte[] buffer = new byte[PACKET_SIZE];
+        
+        // Send command 2 to indicate this is a file
+        outputStream.writeInt(2);
+        outputStream.writeUTF(file.getName());
+        outputStream.writeLong(file.length());
+
+        byte[] buffer = new byte[4096];
         int length;
         while ((length = fis.read(buffer)) > 0) {
-            // Convert the file chunk to a Base64 string and send
-            String data = Base64.getEncoder().encodeToString(Arrays.copyOf(buffer, length)) + "\n"; 
-            outputStream.write(data.getBytes());
+            outputStream.write(buffer, 0, length);
         }
 
         fis.close();
         outputStream.flush();
-
-        // Add a delay before closing the connection
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Close the connection
-        socket.close();
     }
 
     public void sendMessage(String message) throws IOException {
-        String completeMessage = message + "\n"; 
-        outputStream.write(completeMessage.getBytes());
+        // Send command 1 to indicate this is a message
+        outputStream.writeInt(1);  
+        outputStream.writeUTF(message);
         outputStream.flush();
     }
-    
+
+    public void close() throws IOException {
+        outputStream.close();
+        socket.close();
+    }
+
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 6667);
-            Sender sender = new Sender(socket);
-            sender.sendFile("C:\\JavaProjects\\chatclient\\src\\chatclient\\test.zip"); 
-            try {
-                Thread.sleep(5000);  // Wait for a while before closing the connection.
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            Sender sender = new Sender("localhost", 1234);
+            sender.sendMessage("Hello, world!");
+            sender.sendFile("C:\\JavaProjects\\chatclient\\src\\chatclient\\test.zip");
+            sender.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
