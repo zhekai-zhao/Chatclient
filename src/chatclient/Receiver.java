6e0867bffaa4fa574e00b0bf23c0612487d8832c
zhekai-zhao <zhekai1@ualberta.ca>
2023-07-21 16:56:00 -0600

    opt and test package size

--- src/chatclient/Receiver.java
+++ src/chatclient/Receiver.java
@@ -7,6 +7,7 @@ public class Receiver {
     private ServerSocket serverSocket;
     private Socket socket;
     private DataInputStream inputStream;
+    private static final int BUFFER_SIZE = 4096;
 
     public Receiver(int port) throws IOException {
         this.serverSocket = new ServerSocket(port);
@@ -19,12 +20,30 @@ public class Receiver {
             while (true) {
                 int command = inputStream.readInt();
 
-                if (command == 1) {  // This is a message
-                    String message = inputStream.readUTF();
-                    System.out.println("Received message: " + message);
-                } else if (command == 2) {  // This is a file
-                    receiveFile();
-                    System.out.println("File received.");
+                switch (command) {
+                    case 0:  // Connection established
+                        String helloMessage = inputStream.readUTF();
+                        System.out.println("Connection message: " + helloMessage);
+                        break;
+
+                    case 1:  // This is a message
+                        String message = inputStream.readUTF();
+                        System.out.println("Received message: " + message);
+                        break;
+                    
+                    case 2:  // This is a file
+                        receiveFile();
+                        System.out.println("File received.");
+                        break;
+
+                    case -1:  // Connection terminated
+                        String goodbyeMessage = inputStream.readUTF();
+                        System.out.println("Termination message: " + goodbyeMessage);
+                        return;  // Exit the loop on receiving termination command
+
+                    default:
+                        System.err.println("Unknown command received: " + command);
+                        break;
                 }
             }
         } catch (IOException e) {
@@ -32,17 +51,35 @@ public class Receiver {
         }
     }
 
+
     public void receiveFile() throws IOException {
         String fileName = inputStream.readUTF();
         long fileSize = inputStream.readLong();
-        byte[] fileData = new byte[(int) fileSize];
-        inputStream.readFully(fileData);
 
         File file = new File(fileName);
         FileOutputStream fos = new FileOutputStream(file);
-        fos.write(fileData);
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        long totalBytesRead = 0;
+
+        long startTime = System.currentTimeMillis(); // Start
+
+        while (totalBytesRead < fileSize) {
+            int bytesRead = inputStream.read(buffer);
+            fos.write(buffer, 0, bytesRead);
+            totalBytesRead += bytesRead;
+        }
+
+        long endTime = System.currentTimeMillis(); // End
+
         fos.close();
+
+        long duration = endTime - startTime; // Calculate time
+        System.out.println("File received in: " + duration + " milliseconds.");
     }
+
+
+
 
     public void close() throws IOException {
         inputStream.close();
--- src/chatclient/Sender.java
+++ src/chatclient/Sender.java
@@ -6,30 +6,57 @@ import java.net.Socket;
 public class Sender {
     private Socket socket;
     private DataOutputStream outputStream;
+    private static final int BUFFER_SIZE = 4096;
+
 
     public Sender(String address, int port) throws IOException {
         this.socket = new Socket(address, port);
         this.outputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
     }
+    
+    public void openConnection() throws IOException {
+        // Send command 0 to indicate the start of a connection
+        outputStream.writeInt(0);
+        outputStream.writeUTF("Hello from Sender!");
+        outputStream.flush();
+    }
+
+    public void closeConnection() throws IOException {
+        // Send command -1 to indicate the end of a connection
+        outputStream.writeInt(-1);
+        outputStream.writeUTF("Goodbye from Sender!");
+        outputStream.flush();
+    }
 
     public void sendFile(String filePath) throws IOException {
         File file = new File(filePath);
         FileInputStream fis = new FileInputStream(file);
-        
+
         // Send command 2 to indicate this is a file
         outputStream.writeInt(2);
         outputStream.writeUTF(file.getName());
         outputStream.writeLong(file.length());
 
-        byte[] buffer = new byte[4096];
-        int length;
-        while ((length = fis.read(buffer)) > 0) {
-            outputStream.write(buffer, 0, length);
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead;
+
+        long startTime = System.currentTimeMillis(); // Start!
+
+        while ((bytesRead = fis.read(buffer)) != -1) {
+            outputStream.write(buffer, 0, bytesRead);
         }
+
+        long endTime = System.currentTimeMillis(); // End!
 
         fis.close();
         outputStream.flush();
+
+        long duration = endTime - startTime; // Calculate time
+        System.out.println("File sent in: " + duration + " milliseconds.");
     }
+
+
+
 
     public void sendMessage(String message) throws IOException {
         // Send command 1 to indicate this is a message
@@ -46,8 +73,10 @@ public class Sender {
     public static void main(String[] args) {
         try {
             Sender sender = new Sender("localhost", 1234);
+            sender.openConnection();
             sender.sendMessage("Hello, world!");
             sender.sendFile("C:\\JavaProjects\\chatclient\\src\\chatclient\\test.zip");
+            sender.closeConnection();
             sender.close();
         } catch (IOException e) {
             e.printStackTrace();
